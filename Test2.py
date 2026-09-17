e1aa8f768428ed69bf042095d025ef5127c6cc9a
medhasWorld <pradeepshopgroup@gmail.com>
2026-09-17 12:43:29 -0700

    Add files via upload

---
+++ Test2.py
@@ -0,0 +1,7 @@
+secret = 25
+g = int(input("Guess the number I am thinking about(It is less that 50): "))
+t = 0
+while t == 0 in range(0,5):
+    i = 1
+    t = i
+    print("how many times u have tried: ", t)
